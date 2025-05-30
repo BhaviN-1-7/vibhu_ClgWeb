1fa6c84df1e1bee0fa25a108cd08d4af7e65914e
Kotha Srivibhu <kothasrivibhu.23bds1057@gmail.com>
2025-05-30 21:57:19 +0530

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './Components/Navbar'
 import About from './Components/About'
 import MI from './Components/MI'
