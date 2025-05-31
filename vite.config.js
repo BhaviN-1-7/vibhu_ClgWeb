46ae4b1d5906cb357019e5d2508ff083cea6ae19
BhaviN-1-7 <pachabhavinkumar@gmail.com>
2025-05-31 12:18:00 +0530

    Update configuration for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Bsjrdegreecollege/',
   plugins: [react()],
+  base: '/vibhu_ClgWeb/',
   esbuild: {
     loader: 'jsx',
     include: /src\/.*\.jsx?$/,
